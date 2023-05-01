3ad25b1a2f15c9fa64fe2652a3e608b8e7f18444
Somov Dmitry <37120177+dmsomov@users.noreply.github.com>
2023-05-02 00:00:50 +0300

    test

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,9 +2,14 @@ import React, { memo } from "react";
 
 import { Wrapper, Title } from "./layout.styles";
 
-export const Layout = memo(({ children, title }) => (
-  <Wrapper>
-    <Title>{title}</Title>
-    {children}
-  </Wrapper>
-));
+export const Layout = memo(({ children, title }) => {
+  console.log('111111');
+  console.log('222222');
+  console.log('333333');
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      {children}
+    </Wrapper>
+  );
+});
--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -3,17 +3,33 @@ import ContentLoader from "react-content-loader";
 
 import { Wrapper } from "./loading.styles";
 
-export const Loading = () => (
-  <Wrapper>
-    <ContentLoader
-      viewBox="0 0 400 160"
-      height={160}
-      width={400}
-      backgroundColor="transparent"
-    >
-      <circle cx="150" cy="86" r="8" />
-      <circle cx="194" cy="86" r="8" />
-      <circle cx="238" cy="86" r="8" />
-    </ContentLoader>
-  </Wrapper>
-);
+export const Loading = () => {
+  console.log("222222");
+  console.log("333333");
+  console.log("444444");
+  console.log("555555");
+  return (
+    <Wrapper>
+      <ContentLoader
+        viewBox="0 0 400 160"
+        height={160}
+        width={400}
+        backgroundColor="transparent"
+      >
+        <circle cx="150" cy="86" r="8" />
+        <circle cx="194" cy="86" r="8" />
+        <circle cx="238" cy="86" r="8" />
+      </ContentLoader>
+      <ContentLoader
+        viewBox="0 0 400 160"
+        height={160}
+        width={400}
+        backgroundColor="transparent"
+      >
+        <circle cx="150" cy="86" r="8" />
+        <circle cx="194" cy="86" r="8" />
+        <circle cx="238" cy="86" r="8" />
+      </ContentLoader>
+    </Wrapper>
+  );
+};
