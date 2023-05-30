37cbb3b81d3f3747199ea7105182d4610eb33f57
dmsomov <somovdmitriy@icloud.com>
2023-05-30 15:47:24 +0300

    update

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -3,7 +3,8 @@ import ContentLoader from "react-content-loader";
 
 import { Wrapper } from "./loading.styles";
 
-export const Loading = () => (
+export const Loading = () => {
+  return (
   <Wrapper>
     <ContentLoader
       viewBox="0 0 400 160"
@@ -16,4 +17,4 @@ export const Loading = () => (
       <circle cx="238" cy="86" r="8" />
     </ContentLoader>
   </Wrapper>
-);
+)};
