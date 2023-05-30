82cb554c6da9e89bb3662394d8ab52e075cb17de
dmsomov <somovdmitriy@icloud.com>
2023-05-30 16:02:41 +0300

    update

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -4,28 +4,30 @@ import ContentLoader from "react-content-loader";
 import { Wrapper } from "./loading.styles";
 
 export const Loading = () => {
-  console.log('111111');
+  console.log("222222");
+  console.log("333333");
   return (
-  <Wrapper>
-    <ContentLoader
-      viewBox="0 0 400 160"
-      height={160}
-      width={400}
-      backgroundColor="transparent"
-    >
-      <circle cx="150" cy="86" r="8" />
-      <circle cx="194" cy="86" r="8" />
-      <circle cx="238" cy="86" r="8" />
-    </ContentLoader>
-    <ContentLoader
-      viewBox="0 0 400 160"
-      height={160}
-      width={400}
-      backgroundColor="transparent"
-    >
-      <circle cx="150" cy="86" r="8" />
-      <circle cx="194" cy="86" r="8" />
-      <circle cx="238" cy="86" r="8" />
-    </ContentLoader>
-  </Wrapper>
-)};
+    <Wrapper>
+      <ContentLoader
+        viewBox="0 0 400 160"
+        height={160}
+        width={400}
+        backgroundColor="transparent"
+      >
+        <circle cx="150" cy="86" r="8" />
+        <circle cx="194" cy="86" r="8" />
+        <circle cx="238" cy="86" r="8" />
+      </ContentLoader>
+      <ContentLoader
+        viewBox="0 0 400 160"
+        height={160}
+        width={400}
+        backgroundColor="transparent"
+      >
+        <circle cx="150" cy="86" r="8" />
+        <circle cx="194" cy="86" r="8" />
+        <circle cx="238" cy="86" r="8" />
+      </ContentLoader>
+    </Wrapper>
+  );
+};
