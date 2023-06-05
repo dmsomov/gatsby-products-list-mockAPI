bf71a79b9b80275716741e2abe32e4a7a98fa676
dmsomov <somovdmitriy@icloud.com>
2023-06-05 09:51:37 +0300

    t

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,9 +2,11 @@ import React, { memo } from "react";
 
 import { Wrapper, Title } from "./layout.styles";
 
-export const Layout = memo(({ children, title }) => (
+export const Layout = memo(({ children, title }) => {
+  console.log('1111');
+  return (
   <Wrapper>
     <Title>{title}</Title>
     {children}
   </Wrapper>
-));
+)});
