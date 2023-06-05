a65a3495e76437fa53bba19b4c458cb89bd86dd7
dmsomov <somovdmitriy@icloud.com>
2023-06-05 13:42:10 +0300

    t1

--- src/components/product-card/product-card.jsx
+++ src/components/product-card/product-card.jsx
@@ -3,7 +3,9 @@ import React, { memo } from "react";
 import { Wrapper, Link, Title, Price, Image } from "./product-card.styles";
 
 export const ProductCard = memo(
-  ({ product: { image, name, title, price, id } }) => (
+  ({ product: { image, name, title, price, id } }) => {
+    console.log('1111');
+    return (
     <Wrapper>
       <Image src={image} alt={name}/>
       <Title title={title}>{title}</Title>
@@ -12,5 +14,5 @@ export const ProductCard = memo(
         Learn more about {title}
       </Link>
     </Wrapper>
-  )
+  )}
 );
