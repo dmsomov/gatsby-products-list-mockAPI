7e717be8f843306c30accbce02962d22a3c4f014
dmsomov <somovdmitriy@icloud.com>
2023-06-06 17:28:27 +0300

    qw

--- src/components/loading/loading.jsx
+++ src/components/loading/loading.jsx
@@ -9,6 +9,7 @@ export const Loading = () => {
   console.log("444444");
   console.log("555555");
   console.log("555555");
+  console.log("555555");
   return (
     <Wrapper>
       <ContentLoader
