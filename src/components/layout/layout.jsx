f89e1a3ef8cf900c74997abf965d1a82b7e49fbd
dmsomov <somovdmitriy@icloud.com>
2023-06-13 23:42:19 +0300

    a

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,7 +4,7 @@ import { Wrapper, Title } from "./layout.styles";
 
 export const Layout = memo(({ children, title }) => (
   <Wrapper>
-    <Title>{title}</Title>
+    <Title>{title}!</Title>
     {children}
   </Wrapper>
 ));
