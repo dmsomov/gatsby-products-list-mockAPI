5a71b8e141551a4c8022368b840fc28da6bf59dc
dmsomov <somovdmitriy@icloud.com>
2023-06-13 23:42:41 +0300

    a

--- src/components/error-info/error-info.jsx
+++ src/components/error-info/error-info.jsx
@@ -3,5 +3,5 @@ import React from "react";
 import { Wrapper } from "./error-info.styles";
 
 export const ErrorInfo = ({ errorMessage }) => (
-  <Wrapper>Error: {errorMessage}</Wrapper>
+  <Wrapper>Error: {errorMessage}!</Wrapper>
 );
