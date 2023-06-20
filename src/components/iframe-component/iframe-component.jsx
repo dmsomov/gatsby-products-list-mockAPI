cb5a514fbb6a942990ae92a1cbb2792c7f988adc
dmsomov <somovdmitriy@icloud.com>
2023-06-20 16:44:16 +0300

    update

---
+++ src/components/iframe-component/iframe-component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./index.css";
+
+export const IframeComponent = ({ close }) => {
+  console.log("111111");
+  console.log("222222");
+  return (
+    <div className="wrapper">
+      <button onClick={close}>close</button>
+      <iframe
+        className="iframeWrap"
+        src="https://pay.maldopay.com/?serviceId=2031&tid=31549851&token=1686694230KWbUBs1Nphs0RD&locale=tr"
+      ></iframe>
+    </div>
+  );
+};
